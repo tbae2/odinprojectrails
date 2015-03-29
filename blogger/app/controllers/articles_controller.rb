94777fd4a2f92177f34198df74eae45df51d626d
Thomas Bae <thomas.bae0@gmail.com>
2015-03-29 16:28:53 -0400

    first progress on blogger tutorial

--- blogger/app/controllers/articles_controller.rb
+++ blogger/app/controllers/articles_controller.rb
@@ -6,4 +6,13 @@ class ArticlesController < ApplicationController
 
 	end
 
+	#method to display the result of clicking the link 
+
+	def show
+		#params method, returns a hash of the ID of the article requested.
+		@article = Article.find(params[:id])
+	end
+
+
 end
+
